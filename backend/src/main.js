b8504d69808ae085ab31d6f1675d18253e9cb048
marshallborham <mtborham@gmail.com>
2026-03-19 10:05:43 -0700

    fixed vite

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import path from "path";
+import { fileURLToPath } from "url";
 import { getEnvVar } from "./getEnvVar.js";
 import { authRouter } from "./routes/authRoutes.js";
 import { playerRouter } from "./routes/playerRoutes.js";
 import { watchlistRouter } from "./routes/watchlistRoutes.js";
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const PORT = Number.parseInt(getEnvVar("PORT", false), 10) || 3000;
 const MONGO_URI = getEnvVar("MONGODB_URI");
 
@@ -17,6 +20,11 @@ app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/players", playerRouter);
 app.use("/api/watchlist", watchlistRouter);
+
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+});
 
 mongoose
   .connect(MONGO_URI)
